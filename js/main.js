8ccdd96f5b632cebe5eb6a0316f8f3240476d683
r3hab-media <jared.newnam@gmail.com>
2021-02-25 20:36:15 -0700

    updated code and added new pages

--- js/main.js
+++ js/main.js
@@ -2,25 +2,24 @@
 	animals = ["dogs,", " cats,", " birds,", " fish,", " arachnids,", " and amphibians."];
 
 	for (i = 0; i < animals.length; i++) {
-		if (document.querySelector('.viewAnimals') != null) {
-      document.querySelector(".viewAnimals").innerHTML += animals[i];
-    }
+		if (document.querySelector(".viewAnimals") != null) {
+			document.querySelector(".viewAnimals").innerHTML += animals[i];
+		}
 	}
-}());
-
+})();
 
 //cats * dogs redirect
 (function redirectURL() {
-  let currentURL = window.location.href;
-  if (typeof redirect == 'undefined') {
-    return;
-  }
-  if (currentURL.indexOf('dogs.html') > -1) {
-    redirect.innerHTML = `<a href="/pets/cats.html">Cats</a>`;
-  } else {
-    redirect.innerHTML = `<a href="/pets/dogs.html">Dogs</a>`;
-  }
-}());
+	let currentURL = window.location.href;
+	if (typeof redirect == "undefined") {
+		return;
+	}
+	if (currentURL.indexOf("dogs.html") > -1) {
+		redirect.innerHTML = `<a href="/pets/cats.html">Cats</a>`;
+	} else {
+		redirect.innerHTML = `<a href="/pets/dogs.html">Dogs</a>`;
+	}
+})();
 
 //dogs array
 (function dogArray() {
@@ -38,12 +37,66 @@
 	];
 
 	for (i = 0; i < dogs.length; i++) {
-    if (document.querySelector('.dogs') != null) {
-      document.querySelector(".dogs").innerHTML += '<i class="fas fa-dog"></i> ' + dogs[i] + "<br>";
-    }
+		if (document.querySelector(".dogs") != null) {
+			document.querySelector(".dogs").innerHTML += '<i class="fas fa-dog"></i> ' + dogs[i] + "<br>";
+		}
 	}
-}());
+})();
 
+//get ip, city, state & country
+$.get(
+	"http://ipinfo.io",
+	function (response) {
+		$("#city").html(response.city);
+		$("#state").html(response.region);
+		$("#zip").html(response.postal);
+		// $("#details").html(JSON.stringify(response, null, 4));
+	},
+	"jsonp"
+);
+
+//collapsible
+const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+
+window.addEventListener(
+	"click",
+	(ev) => {
+		const elm = ev.target;
+		if (triggers.includes(elm)) {
+			const selector = elm.getAttribute("data-target");
+			collapse(selector, "toggle");
+		}
+	},
+	false
+);
+
+const fnmap = {
+	toggle: "toggle",
+	show: "add",
+	hide: "remove",
+};
+
+const collapse = (selector, cmd) => {
+	const targets = Array.from(document.querySelectorAll(selector));
+	targets.forEach((target) => {
+		target.classList[fnmap[cmd]]("show");
+	});
+};
+
+window.onload = function () {
+	var inputs = document.getElementsByClassName("sum"),
+		total = document.getElementById("payment-total");
+
+	for (var i = 0; i < inputs.length; i++) {
+		inputs[i].onchange = function () {
+			var add = this.value * (this.checked ? 1 : -1);
+			total.innerHTML = parseFloat(total.innerHTML) + add;
+			var new_total = parseFloat(document.getElementById("input").value);
+			console.log(new_total);
+			document.getElementById("input").value = new_total + add;
+		};
+	}
+};
 
 // date reference
 let d = new Date();
--- js/nav.js
+++ js/nav.js
@@ -14,6 +14,7 @@ let navMenu = `
       <li class="item"><a href="/locations.html">Locations</a></li>
       <li class="item"><a href="/press.html">Press</a></li>
       <li class="item"><a href="/contact.html">Contact Us</a></li>
+      <li class="item"><a href="/js.html">JS</a></li>
       <li class="item button"><a href="#">Log In</a></li>
       <li class="item button secondary"><a href="#">Sign Up</a></li>
       <li class="toggle"><a href="#"><i class="fas fa-bars"></i></a></li>
@@ -30,41 +31,41 @@ const items = document.querySelectorAll(".item");
 
 /* Toggle mobile menu */
 function toggleMenu() {
-  if (menu.classList.contains("active")) {
-    menu.classList.remove("active");
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-  } else {
-    menu.classList.add("active");
-    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-  }
+	if (menu.classList.contains("active")) {
+		menu.classList.remove("active");
+		toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+	} else {
+		menu.classList.add("active");
+		toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+	}
 }
 
 /* Activate Submenu */
 function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
-  } else {
-    this.classList.add("submenu-active");
-  }
+	if (this.classList.contains("submenu-active")) {
+		this.classList.remove("submenu-active");
+	} else if (menu.querySelector(".submenu-active")) {
+		menu.querySelector(".submenu-active").classList.remove("submenu-active");
+		this.classList.add("submenu-active");
+	} else {
+		this.classList.add("submenu-active");
+	}
 }
 
 /* Close Submenu From Anywhere */
 function closeSubmenu(e) {
-  let isClickInside = menu.contains(e.target);
+	let isClickInside = menu.contains(e.target);
 
-  if (!isClickInside && menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-  }
+	if (!isClickInside && menu.querySelector(".submenu-active")) {
+		menu.querySelector(".submenu-active").classList.remove("submenu-active");
+	}
 }
 /* Event Listeners */
 toggle.addEventListener("click", toggleMenu, false);
 for (let item of items) {
-  if (item.querySelector(".submenu")) {
-    item.addEventListener("click", toggleItem, false);
-  }
-  item.addEventListener("keypress", toggleItem, false);
+	if (item.querySelector(".submenu")) {
+		item.addEventListener("click", toggleItem, false);
+	}
+	item.addEventListener("keypress", toggleItem, false);
 }
 document.addEventListener("click", closeSubmenu, false);
